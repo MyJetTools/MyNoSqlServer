f431f5f1596381a1ffb93e68e0a629c82081d958
amigin <amigin@gmail.com>
2020-07-02 17:20:29 +0300

    Cancelation Token for IEnumerable is fixed

--- MyNoSqlServer.TcpContracts/MyNoSqlTcpSerializer.cs
+++ MyNoSqlServer.TcpContracts/MyNoSqlTcpSerializer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using MyTcpSockets;
 using MyTcpSockets.Extensions;
@@ -8,7 +9,7 @@ namespace MyNoSqlServer.TcpContracts
 {
     public class MyNoSqlTcpSerializer : ITcpSerializer<IMyNoSqlTcpContract>
     {
-        public async IAsyncEnumerable<IMyNoSqlTcpContract> DeserializeAsync(TcpDataReader dataReader, CancellationToken ct)
+        public async IAsyncEnumerable<IMyNoSqlTcpContract> DeserializeAsync(TcpDataReader dataReader,[EnumeratorCancellation] CancellationToken ct)
         {
             var result = await SerializerDeserializer.DeserializeAsync(dataReader, ct);
             yield return result;
