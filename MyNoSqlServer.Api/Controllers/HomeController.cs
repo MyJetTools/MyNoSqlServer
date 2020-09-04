db27f74258b4d195b92ca7bc94ff728b14ed94ef
amigin <amigin@gmail.com>
2020-09-04 22:25:42 +0300

    Fixed Script

--- MyNoSqlServer.Api/Controllers/HomeController.cs
+++ MyNoSqlServer.Api/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MyNoSqlServer.Api.Controllers
         public IActionResult Index()
         {
             var version = typeof(HomeController).Assembly.GetName().Version?.ToString() ?? "";
-            return Content("<html><head><title>"+version+" MyNoSQLServer</title><link href=\"/css/bootstrap.css\" rel=\"stylesheet\" type=\"text/css\" /><script src=\"/js/jquery-3.4.1.min.js\"></script><script src=\"/js/main.js?ver="+Guid.NewGuid()+"\"></script></head><body></body></html>", "text/html; charset=UTF-8");
+            return Content("<html><head><title>"+version+" MyNoSQLServer</title><link href=\"/css/bootstrap.css\" rel=\"stylesheet\" type=\"text/css\" /><script src=\"/js/jquery-3.4.1.min.js\"></script><script src=\"/js/main.min.js?ver="+Guid.NewGuid()+"\"></script></head><body></body></html>", "text/html; charset=UTF-8");
         }
         
     }
