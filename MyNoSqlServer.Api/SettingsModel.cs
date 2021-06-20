3f9453077d49f1726082c1ca8583ab637866b539
Andrey <amigin@gmail.com>
2021-06-20 16:05:54 +0300

    Trying to build .net 6

--- MyNoSqlServer.Api/SettingsModel.cs
+++ MyNoSqlServer.Api/SettingsModel.cs
@@ -29,79 +29,72 @@ namespace MyNoSqlServer.Api
     {
         public static SettingsModel LoadSettings()
         {
-            try
+            byte[] settingsContent = null;
+
+
+            const string fileName = ".mynosqlserver";
+
+
+            var homeFolder = Environment.GetEnvironmentVariable("HOME");
+            if (!string.IsNullOrEmpty(homeFolder) && File.Exists(Path.Combine(homeFolder, fileName)))
             {
+                var path = Path.Combine(homeFolder, fileName);
+                settingsContent = File.ReadAllBytes(path);
+            }
+            else
+            {
+                Console.WriteLine("Settings File not found as Linux or MacOs Path. Skipping");
+            }
 
-
-                byte[] settingsContent = null;
-                
-                
-                const string fileName = ".mynosqlserver";
-                
-
-                var homeFolder = Environment.GetEnvironmentVariable("HOME");
+            if (settingsContent == null)
+            {
+                homeFolder = Environment.GetEnvironmentVariable("HOMEDRIVE") +
+                             Environment.GetEnvironmentVariable("HOMEPATH");
                 if (!string.IsNullOrEmpty(homeFolder) && File.Exists(Path.Combine(homeFolder, fileName)))
                 {
-                    var path = Path.Combine(homeFolder, fileName);
-                    settingsContent = File.ReadAllBytes(path);
+                    settingsContent = File.ReadAllBytes(Path.Combine(homeFolder, fileName));
                 }
                 else
                 {
-                    Console.WriteLine("Settings File not found as Linux or MacOs Path. Skipping");
+                    Console.WriteLine("Settings File not found as Windows Path. Skipping");
                 }
-
-                if (settingsContent == null)
-                {
-                    homeFolder = Environment.GetEnvironmentVariable("HOMEDRIVE") + Environment.GetEnvironmentVariable("HOMEPATH");
-                    if (!string.IsNullOrEmpty(homeFolder) && File.Exists(Path.Combine(homeFolder, fileName)))
-                    {
-                        settingsContent = File.ReadAllBytes(Path.Combine(homeFolder, fileName));
-                    }
-                    else
-                    {
-                        Console.WriteLine("Settings File not found as Windows Path. Skipping");
-                    }
-                }
-    
-
-                if (settingsContent == null)
-                {
-                    throw new Exception("Settings file not found at HOME/" + fileName + " path");
-                }
-                
-
-                var settingsModel = MyYamlDeserializer.Deserialize<SettingsModel>(settingsContent);
-
-  
-
-                if (string.IsNullOrEmpty(settingsModel.PersistenceDest))
-                {
-                    Console.WriteLine("No connection string found. Backups are disabled");
-                    Console.WriteLine("In case to enable, please specify 'BackupAzureConnectString' in env variable or in json file: ~/.mynosqlserver");
-                    throw new Exception("PersistencePath should not be Empty");
-                }
-
-                if (!settingsModel.PersistenceDest.StartsWith("http") && !settingsModel.PersistenceDest.StartsWith("DefaultEndpointsProtocol"))
-                {
-                    throw new Exception(
-                        "PersistencePath Parameter should start either from http/https for GRPC Dependency or from DefaultEndpointsProtocol");
-                }
-
-                if (settingsModel.MaxPayloadSize < 1024 * 1024)
-                {
-                    throw new Exception($"MaxPayloadSize settings must be greater then {1024 * 1024}");
-                }
-                
-                Console.WriteLine("Settings are read ok");
-
-                return settingsModel;
             }
-            catch (Exception e)
+
+
+            if (settingsContent == null)
             {
-                Console.WriteLine("Error reading Settings File: "+e.Message);
-                Console.WriteLine("Backups are disabled");
-                return new SettingsModel();
+                throw new Exception("Settings file not found at HOME/" + fileName + " path");
             }
+
+
+            var settingsModel = MyYamlDeserializer.Deserialize<SettingsModel>(settingsContent);
+
+
+
+            if (string.IsNullOrEmpty(settingsModel.PersistenceDest))
+            {
+                Console.WriteLine("No connection string found. Backups are disabled");
+                Console.WriteLine(
+                    "In case to enable, please specify 'BackupAzureConnectString' in env variable or in json file: ~/.mynosqlserver");
+                throw new Exception("PersistencePath should not be Empty");
+            }
+
+            if (!settingsModel.PersistenceDest.StartsWith("http") &&
+                !settingsModel.PersistenceDest.StartsWith("DefaultEndpointsProtocol"))
+            {
+                throw new Exception(
+                    "PersistencePath Parameter should start either from http/https for GRPC Dependency or from DefaultEndpointsProtocol");
+            }
+
+            if (settingsModel.MaxPayloadSize < 1024 * 1024)
+            {
+                throw new Exception($"MaxPayloadSize settings must be greater then {1024 * 1024}");
+            }
+
+            Console.WriteLine("Settings are read ok");
+
+            return settingsModel;
+
         }
     }
     
